35a63f60f9ceda16b583470cc24ae84b9267d4ec
JHades <jhades.dev@gmail.com>
2015-07-28 10:45:18 +0200

    ongoing

--- angular2-forms/app/app.ts
+++ angular2-forms/app/app.ts
@@ -1,5 +1,6 @@
 /// <reference path="typings/_custom.d.ts" />
 import {Component, View, bootstrap} from 'angular2/angular2';
+import {formDirectives} from 'angular2/forms';
 import {RouteConfig, RouterOutlet, RouterLink, routerInjectables} from 'angular2/router';
 
 
@@ -9,7 +10,7 @@ import {RouteConfig, RouterOutlet, RouterLink, routerInjectables} from 'angular2
 })
 @View({
   templateUrl: './app.html?v=<%= VERSION %>',
-  directives: [RouterOutlet, RouterLink]
+  directives: [formDirectives]
 })
 class App {}
 
