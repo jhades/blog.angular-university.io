2c3e3eeae12a7419d2415b7a3675d84328abc3b3
JHades <jhades.dev@gmail.com>
2015-09-18 23:57:01 +0200

    ongoing

--- tmp/app/init.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="typings/_custom.d.ts" />
-System.config({
-  baseURL: '<%= APP_BASE %>',
-  paths: {'*': '*.js?v=<%= VERSION %>'}
-});
-
-System.import('app')
-  .catch(e => console.error(e,
-    'Report this error at https://github.com/mgechev/angular2-seed/issues'));
