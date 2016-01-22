e7aa78b602e4c01331996fd02dbd6cf637aaebe9
JHades <jhades.dev@gmail.com>
2016-01-22 21:27:09 +0100

    ongoing

--- ng2-change-detection/src/ComponentB.ts
+++ ng2-change-detection/src/ComponentB.ts
@@ -1,10 +1,13 @@
 
 import {ChangeDetectionLoggingComponent} from "./ChangeDetectionLoggingComponent";
 import {Component, Input} from "angular2/core";
+import {ComponentD} from "./ComponentD";
 
 @Component({
     selector: 'component-b',
+    directives: [ComponentD],
     template: `<div class="component-b">
+                    <component-d [counter]="counter"></component-d>
                     <div class="counter">B: {{counter}}</div>
                </div>`
 })
---
+++ ng2-change-detection/src/ComponentD.ts
@@ -0,0 +1,19 @@
+
+import {ChangeDetectionLoggingComponent} from "./ChangeDetectionLoggingComponent";
+import {Component, Input} from "angular2/core";
+
+@Component({
+    selector: 'component-d',
+    template: `<div class="component-d">
+                    <div class="counter">D: {{counter}}</div>
+               </div>`
+})
+export class ComponentD extends ChangeDetectionLoggingComponent  {
+
+    @Input() counter = 0;
+
+    constructor() {
+        super('ComponentD');
+    }
+
+}
