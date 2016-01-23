5cf6dc2748d49b4684e216989b073ff4775af4ae
JHades <jhades.dev@gmail.com>
2016-01-23 21:26:15 +0100

    splitting in multiple files

--- ng2-rxjs/src/flux/actions.ts
+++ ng2-rxjs/src/flux/actions.ts
@@ -1,5 +1,23 @@
 
-export class AddTodoAction       { constructor(public todoId: number, public text: string){} }
-export class ToggleTodoAction    { constructor(public id: number){} }
-export class SetVisibilityFilter { constructor(public filter: string){} }
+export class AddTodoAction {
+
+    constructor(public todoId:number, public text:string) {
+
+    }
+}
+
+export class ToggleTodoAction {
+
+    constructor(public id:number) {
+
+    }
+}
+
+export class SetVisibilityFilter {
+
+    constructor(public filter:string) {
+
+    }
+}
+
 export type Action = AddTodoAction|ToggleTodoAction|SetVisibilityFilter;
--- ng2-rxjs/src/flux/app-state.ts
+++ ng2-rxjs/src/flux/app-state.ts
@@ -1,4 +1,4 @@
-import {Todo} from "./Todo";
+import {Todo} from "../Todo";
 
 export interface AppState {
     todos: Todo[];
