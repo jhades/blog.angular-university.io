65cd948705ce39ad0967a033f463b67ebafd4295
JHades <jhades.dev@gmail.com>
2016-02-29 12:04:54 +0100

    ongoing

--- ng2-change-detection/src/app.ts
+++ ng2-change-detection/src/app.ts
@@ -1,7 +1,7 @@
 ///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
 
 import 'angular2/bundles/angular2-polyfills';
-import {Component, NgZone} from 'angular2/core';
+import {Component, NgZone, OnChanges, DoCheck} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
 import {todos as initialData} from './test_data';
 import {TodoList} from "./todo_list";
@@ -22,10 +22,7 @@ export class App {
         ngZone.run(() => {
             console.log('this code will run inside the Angular zone...');
         });
-
-
     }
-
 
 }
 
