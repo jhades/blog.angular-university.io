4b3f95c3200f7aa30d166389aa9230e89a3abd45
JHades <jhades.dev@gmail.com>
2016-03-25 23:11:39 +0100

    custom form control

--- custom-form-component/src/app.ts
+++ custom-form-component/src/app.ts
@@ -11,10 +11,13 @@ import {SearchBoxValueAccessor} from "./search_box_value_accessor";
     directives: [SearchBox, SearchBoxValueAccessor],
     template: `<form [ngFormModel]="form">
 
-                    <input ngControl="username" [(ngModel)]="username"  placeholder="Just an Input"> 
+                    <input ngControl="username" [(ngModel)]="username"  placeholder="Username" required>
+                    <input ngControl="firstName"  placeholder="First Name">
+                    <input [ngFormControl]="lastName" placeholder="Last Name">
+
                     <search-box [label]='"Search 1"' ngControl="search" [(ngModel)]="search"></search-box>
                     <search-box [label]='"Search 2"' [ngFormControl]="anotherSearch"></search-box>
-                    
+       
                     <button (click)="onGo()">Go</button>
         
                 </form>
@@ -27,12 +30,16 @@ export class App {
     username:string;
     search:string;
 
+    lastName = new Control("", Validators.required);
+
     anotherSearch = new Control("",Validators.required);
 
     constructor(private fb: FormBuilder) {
         this.form = fb.group({
             username: ["", Validators.required],
-            search: ["search", Validators.required],
+            firstName: ["", Validators.required],
+            lastName: this.lastName,
+            search: ["", Validators.required],
             anotherSearch: this.anotherSearch
         });
 
